3e5a4fa596ba5bf430d118ce5b857cd4d60bed66
Cherish13 <cherishlyte13@gmail.com>
2023-07-06 16:56:13 -0700

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Task 1 module
+'''
+
+def concat(str1: str, str2: str) -> str:
+    '''concat function takes two string arguments and returns their concatenated string
+    '''
+    return str1 + str2
