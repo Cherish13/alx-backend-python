c31730565531644c6d03da0c51ba1776f3b98ef8
Cherish13 <cherishlyte13@gmail.com>
2023-07-06 17:36:52 -0700

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''task 6 module.
 '''
-from typing import List
+from typing import List, Union
 
 
-def sum_list(input_list: List[float]) -> float:
-    return sum(input_list)
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    return sum(mxd_lst)
